f70a3b20237298b18854427c3f86206580662e28
kshen3778 <kshen3778@gmail.com>
2017-12-21 17:42:31 -0500

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <div v-for="(value, key) in products">
+        {{ key }}: {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+
+  name: 'HelloWorld',
+  data () {
+    return {
+      products: {}
+    }
+  },
+
+  created() {
+      this.fetchData()
+  },
+
+  methods: {
+      fetchData() {
+        axios.get('https://hardwarerental-kshen3778.c9users.io/dashboard')
+        .then((resp) => {
+          this.products = resp.data
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
+  }
+}
+
+</script>
