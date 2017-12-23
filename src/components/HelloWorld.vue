9ec53fc6fa2dab2890bde1e2a8da2ff3a787ff57
kshen3778 <kshen3778@gmail.com>
2017-12-23 11:32:36 -0500

    Added confirm refund buttons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="hello">
     <div v-for="(value, key) in products">
-        {{ key }}: {{ value }}
+        ID: {{key}}  |  Name: {{value.name}}  |  Deposit: {{value.depositValue}}
+        <div v-if="value.owner">
+          Owner: {{value.owner.id}}
+          <br>
+          Returned: {{value.owner.returned}}
+          <div v-if="value.owner.returned == true">
+            <button type="button" v-on:click="refund(key)">Confirm Return</button>
+          </div>
+        </div>
+        <br>
     </div>
   </div>
 </template>
@@ -27,6 +36,19 @@ export default {
         .then((resp) => {
           this.products = resp.data
           console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      },
+
+      refund(id) {
+        axios.post('https://hardwarerental-kshen3778.c9users.io/refund', {
+          itemidfield: id
+        })
+        .then((resp) => {
+          console.log(resp)
+          this.fetchData();
         })
         .catch((err) => {
           console.log(err)
