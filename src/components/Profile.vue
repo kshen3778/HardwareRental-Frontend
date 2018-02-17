f70f67166776ca188e05b7123bfe507916ee4054
kshen3778 <kshen3778@gmail.com>
2018-02-17 12:35:39 -0500

    Added card update and email verification

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,6 +5,13 @@
     Email: {{user.email}}
     <br>
     Update Card:
+    <br>
+    <card class='stripe-card'
+      stripe='pk_test_p6Wk8cKlavPdUlqqsF9s4u1m'
+      :options='stripeOptions'
+    />
+
+    <button class='pay-with-stripe' @click='updateCard'>Update Card</button>
   </div>
 
   <div v-else-if="!user">
@@ -15,12 +22,16 @@
 
 <script>
 import axios from 'axios'
+import { Card, createToken } from 'vue-stripe-elements'
 export default {
 
   name: 'Profile',
   data () {
     return {
-      user: null
+      user: null,
+      stripeOptions: {
+        // see https://stripe.com/docs/stripe.js#element-options for details
+      }
     }
   },
 
@@ -35,8 +46,34 @@ export default {
     })
   },
 
-  methods: {
+  components: { Card },
 
+  methods: {
+    updateCard () {
+      //check out https://forum.vuejs.org/t/how-can-i-get-the-data-returned-by-callback-function/18433
+
+      var self = this;
+
+      // createToken returns a Promise which resolves in a result object with
+      // either a token or an error key.
+      // See https://stripe.com/docs/api#tokens for the token object.
+      // See https://stripe.com/docs/api#errors for the error object.
+      // More general https://stripe.com/docs/stripe.js#stripe-create-token.
+      createToken().then(function(data){
+        console.log(data.token);
+        axios.post('https://hardwarerental-kshen3778.c9users.io/updateCard', {
+          stripeToken: data.token.id
+        })
+        .then((resp) => {
+          console.log(resp);
+          self.response = resp.data;
+          //redirect to the profile page
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      });
+    }
   }
 }
 
